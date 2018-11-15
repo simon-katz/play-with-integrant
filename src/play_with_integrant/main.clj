c2bef6ca88474629b1ea2ade78cc5e550090e447
Simon Katz <sk@nomistech.com>
2018-11-15 12:56:05 +0000

    Add some logging

--- src/play_with_integrant/main.clj
+++ src/play_with_integrant/main.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [integrant.core :as ig]
             [ring.adapter.jetty :as jetty]
-            [ring.util.response :as resp]))
+            [ring.util.response :as resp]
+            [taoensso.timbre :as log]))
 
 ;;;; ___________________________________________________________________________
 
@@ -12,17 +13,20 @@
 ;;;; ___________________________________________________________________________
 
 (defmethod ig/init-key :adapter/jetty [_ {:keys [handler] :as opts}]
+  (log/info "Initializing :adapter/jetty .")
   (jetty/run-jetty handler
                    (-> opts
                        (dissoc :handler)
                        (assoc :join? false))))
 
 (defmethod ig/halt-key! :adapter/jetty [_ server]
+  (log/info "Halting :adapter/jetty .")
   (.stop server))
 
 ;;;; ___________________________________________________________________________
 
 (defmethod ig/init-key :handler/greet [_ {:keys [name]}]
+  (log/info "Initializing :handler/greet .")
   (fn [_] (resp/response (str "Hello " name))))
 
 ;;;; Note that we don't need to define a halt-key! for `:handler/greet`.
