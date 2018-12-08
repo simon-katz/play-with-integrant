3209ff16990979baddbc9a2bc937c18fc6ff3b4a
Simon Katz <sk@nomistech.com>
2018-12-08 17:37:42 +0000

    Remove commented-out dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,10 @@
 (defproject play-with-integrant "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;; [com.fzakaria/slf4j-timbre "0.3.8"]
-                 [com.nomistech/clj-utils "0.9.0"]
+  :dependencies [[com.nomistech/clj-utils "0.9.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [integrant "0.7.0"]
                  [org.clojure/clojure "1.9.0"]
-                 ;; [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 ;; [org.slf4j/jul-to-slf4j "1.7.25"]
-                 ;; [org.slf4j/log4j-over-slf4j "1.7.25"]
                  [ring "1.7.1"]]
   :main play-with-integrant.system.main
   :target-path "target/%s"
