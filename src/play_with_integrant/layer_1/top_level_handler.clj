db6a5ae36ac0f0fed47024c6ee37a0b048c0daec
Simon Katz <sk@nomistech.com>
2018-12-08 14:58:58 +0000

    Rename `layer-1.handlers` -> `layer-1.top-level-handler`

--- src/play_with_integrant/layer_1/top_level_handler.clj
+++ src/play_with_integrant/layer_1/top_level_handler.clj
@@ -1,4 +1,4 @@
-(ns play-with-integrant.layer-1.handlers
+(ns play-with-integrant.layer-1.top-level-handler
   (:require [clojure.string :as str]
             [integrant.core :as ig]
             [ring.util.response :as resp]
@@ -9,9 +9,9 @@
             ["Hello there,"
              name]))
 
-(defmethod ig/init-key :play-with-integrant.layer-1/handlers
+(defmethod ig/init-key :play-with-integrant.layer-1/top-level-handler
   [_ {:keys [name]}]
-  (log/info "Initializing `:play-with-integrant.layer-1/handlers`.")
+  (log/info "Initializing `:play-with-integrant.layer-1/top-level-handler`.")
   (fn [_] (resp/response (str/join " "
                                    ["[Less-dynamic string]"
                                     (make-greeting name)]))))
