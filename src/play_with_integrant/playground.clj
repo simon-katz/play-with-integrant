8f55bd9017410cccfdf285f1040821ef08842e96
Simon Katz <sk@nomistech.com>
2018-12-08 20:55:09 +0000

    Add keyword hierarchy example

--- src/play_with_integrant/playground.clj
+++ src/play_with_integrant/playground.clj
@@ -14,17 +14,23 @@
   (log/info "Halting `play-with-integrant.playground/key-001`.")
   :this-value-is-ignored-which-means-you-cannot-change-the-values-in-the-system)
 
+;;;; ___________________________________________________________________________
+
 ;;;; TODO Add `ig/suspend-key!` and `ig/resume-key` examples.
 ;;;; TODO Add `ig/prep-key!` examples.
 
 ;;;; ___________________________________________________________________________
+;;;; Keyword hierarchy example.
 
-(defmethod ig/init-key :play-with-integrant.playground/key-002
+(derive :play-with-integrant.playground/key-002
+        :play-with-integrant.playground/super-of-key-002)
+
+(defmethod ig/init-key :play-with-integrant.playground/super-of-key-002
   [k v]
-  (log/info "Initializing `play-with-integrant.playground/key-002`.")
+  (log/info "Initializing `:play-with-integrant.playground/super-of-key-002`.")
   {:wrapped-by-init v})
 
-(defmethod ig/halt-key! :play-with-integrant.playground/key-002
+(defmethod ig/halt-key! :play-with-integrant.playground/super-of-key-002
   [k v]
-  (log/info "Halting `play-with-integrant.playground/key-002`.")
+  (log/info "Halting `:play-with-integrant.playground/super-of-key-002`.")
   :this-value-is-ignored-which-means-you-cannot-change-the-values-in-the-system)
