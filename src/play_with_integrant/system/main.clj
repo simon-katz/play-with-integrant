868b481a95be9aa3266960963d872d70a6f11d49
Simon Katz <sk@nomistech.com>
2018-12-08 23:25:48 +0000

    Call `ig/prep`

--- src/play_with_integrant/system/main.clj
+++ src/play_with_integrant/system/main.clj
@@ -4,5 +4,6 @@
             [play-with-integrant.system.config-for-ig :as config-for-ig]))
 
 (defn -main []
-  (let [config-for-ig (config-for-ig/read-config-loading-namespaces)]
-    (ig/init config-for-ig)))
+  (-> (config-for-ig/read-config-loading-namespaces)
+      ig/prep
+      ig/init))
